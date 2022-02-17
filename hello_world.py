9e10aa0f7c8078eb4c78ff36e00f3c5c5681eac3
Devin Jhu <devin.jhu@mths.ca>
2022-02-17 19:45:47 +0000

    The proper format Hello, World!

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created by Devin Jhu
+# Created on February 2022
+# The proper formated "Hello, World!"
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
